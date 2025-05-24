be8527ce0a2cc2532a950654f67bb01f873a93f2
Seungho-Min <msh907@naver.com>
2025-05-24 22:08:35 +0900

    msh first commit

---
+++ kmu_lms_project/views.py
@@ -0,0 +1,21 @@
+# main/views.py
+from django.shortcuts import render
+from notices.models import Notice
+from library_info.models import LibraryInfo
+import datetime
+
+def home_view(request):
+    notices = Notice.objects.order_by('-posted_date')[:3]  # 최신 3건
+
+    # 오늘 요일에 맞는 도서관 정보
+    day_map = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    today_key = day_map[datetime.datetime.today().weekday()]
+    try:
+        today_info = LibraryInfo.objects.get(day=today_key)
+    except LibraryInfo.DoesNotExist:
+        today_info = None
+
+    return render(request, 'main/home.html', {
+        'notices': notices,
+        'today_info': today_info,
+    })
